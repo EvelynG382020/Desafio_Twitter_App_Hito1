c35d1aaab0e1224be4bcd130f8de39211cd42615
evelyn gomez <evelyngomezfrez@gmail.com>
2021-05-12 14:42:56 -0400

    array whith hashtag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def like(tweet, user)
         tweet.likes.find{|like| like.user_id == user.id}
     end
+
+    # def pre_follow
+    #     current_user.friends.find { |friend| friend.friend_id == tweet.user.id }
+    # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friends, dependent: :destroy
+  paginates_per 50
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
